da16f5fca01f4fe162bd300ff26a68aa2333aa1e
Adderin <adderin@gmail.com>
2020-04-09 13:41:14 +0200

    Updates

---
+++ precache-manifest.4723114e64234bb3e724f92e20dfa529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3ccb484c17234a9cbd3a58c9982152",
+    "url": "/D3Show-gh/index.html"
+  },
+  {
+    "revision": "467f2e87041fa8f6e0b8",
+    "url": "/D3Show-gh/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c15811c79fb615d6d74",
+    "url": "/D3Show-gh/static/js/2.47c1a01c.chunk.js"
+  },
+  {
+    "revision": "3440a548119abca954b3d79a75843c92",
+    "url": "/D3Show-gh/static/js/2.47c1a01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467f2e87041fa8f6e0b8",
+    "url": "/D3Show-gh/static/js/main.3dc624b6.chunk.js"
+  },
+  {
+    "revision": "da7b01d2c879d48457f7",
+    "url": "/D3Show-gh/static/js/runtime-main.27e60b53.js"
+  }
+]);
